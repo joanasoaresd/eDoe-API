33fcc487fd0118f4d1c5a1b04bf068230dc77843
joanasoaresd <joana.darck@dcx.ufpb.br>
2021-10-26 01:40:49 -0300

    #1 - criando projeto e adicionando dependencias basicas

---
+++ src/main/java/br/ufpb/edoe/EdoeApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.edoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufpb/edoe/EdoeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.edoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

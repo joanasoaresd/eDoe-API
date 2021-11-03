6599961257a240bb10d51feaa9c8d85db5091aa2
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-11-03 13:46:29 -0300

    #8 Adição do Filtro Jwt para validar quais endpoints precisam de autorização

--- src/main/java/br/ufpb/edoe/EdoeApplication.java
+++ src/main/java/br/ufpb/edoe/EdoeApplication.java
@@ -2,12 +2,24 @@ package br.ufpb.edoe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import br.ufpb.edoe.filter.JWTFilter;
 
 @SpringBootApplication
 public class EdoeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdoeApplication.class, args);
-	}
+  @Bean
+  public FilterRegistrationBean<JWTFilter> filterJwt() {
+    FilterRegistrationBean<JWTFilter> filterRB = new FilterRegistrationBean<>();
+    filterRB.setFilter(new JWTFilter());
+    filterRB.addUrlPatterns("/users/role");
+    return filterRB;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(EdoeApplication.class, args);
+  }
 
 }

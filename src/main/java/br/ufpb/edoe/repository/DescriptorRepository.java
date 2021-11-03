5a540b15c794d5750e6e2b82c8ce3194441639c9
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-11-03 14:04:35 -0300

    #18 Alteração do tipo de retorno: Optional -> List

--- src/main/java/br/ufpb/edoe/repository/DescriptorRepository.java
+++ src/main/java/br/ufpb/edoe/repository/DescriptorRepository.java
@@ -2,12 +2,12 @@ package br.ufpb.edoe.repository;
 
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.ufpb.edoe.entity.Descriptor;
 
 @Repository
-public interface DescriptorRepository extends JpaRepository<Descriptor, Integer> {
-    Optional<Descriptor> findByName(String name);
+public interface DescriptorRepository extends JpaRepository<Descriptor, String> {
+    List<Descriptor> findByNameContainingIgnoreCase(String name);
 }

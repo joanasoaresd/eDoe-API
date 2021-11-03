c0e7c92ed1b62c1c6eafe1678e9506eace37497f
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-11-03 13:41:57 -0300

    #7, #8 Exceptions criadas adicionadas ao handler

--- src/main/java/br/ufpb/edoe/exceptions/advice/RestExceptionHandler.java
+++ src/main/java/br/ufpb/edoe/exceptions/advice/RestExceptionHandler.java
@@ -5,15 +5,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.ufpb.edoe.exceptions.DescriptorExistsException;
+import br.ufpb.edoe.exceptions.InvalidUserRoleException;
 import br.ufpb.edoe.exceptions.UserExistsException;
+import br.ufpb.edoe.exceptions.UserNotFoundException;
+import br.ufpb.edoe.exceptions.UserNotLoggedException;
+
 import org.springframework.http.HttpStatus;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-@ResponseStatus(HttpStatus.CONFLICT)
-@ExceptionHandler(UserExistsException.class)
-public ErrorResponseBody handleUserExistsException(UserExistsException e) {
+  @ResponseStatus(HttpStatus.CONFLICT)
+  @ExceptionHandler(UserExistsException.class)
+  public ErrorResponseBody handleUserExistsException(UserExistsException e) {
     ErrorResponseBody error = new ErrorResponseBody();
     error.setStatus(e.getErrorStatus());
     error.setOrigin(e.getErrorOrigin());
@@ -21,14 +25,44 @@ public ErrorResponseBody handleUserExistsException(UserExistsException e) {
     return error;
   }
 
-@ResponseStatus(HttpStatus.CONFLICT)
-@ExceptionHandler(DescriptorExistsException.class)
-public ErrorResponseBody handleDescriptorExistsException(DescriptorExistsException e) {
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(UserNotFoundException.class)
+  public ErrorResponseBody handleUserNotFoundException(UserNotFoundException e) {
     ErrorResponseBody error = new ErrorResponseBody();
     error.setStatus(e.getErrorStatus());
     error.setOrigin(e.getErrorOrigin());
     error.setDetail(e.getMessage());
     return error;
   }
-    
+
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  @ExceptionHandler(UserNotLoggedException.class)
+  public ErrorResponseBody handleUserNotLoggedException(UserNotLoggedException e) {
+    ErrorResponseBody error = new ErrorResponseBody();
+    error.setStatus(e.getErrorStatus());
+    error.setOrigin(e.getErrorOrigin());
+    error.setDetail(e.getMessage());
+    return error;
+  }
+
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  @ExceptionHandler(InvalidUserRoleException.class)
+  public ErrorResponseBody handleInvalidUserRoleException(InvalidUserRoleException e) {
+    ErrorResponseBody error = new ErrorResponseBody();
+    error.setStatus(e.getErrorStatus());
+    error.setOrigin(e.getErrorOrigin());
+    error.setDetail(e.getMessage());
+    return error;
+  }
+
+  @ResponseStatus(HttpStatus.CONFLICT)
+  @ExceptionHandler(DescriptorExistsException.class)
+  public ErrorResponseBody handleDescriptorExistsException(DescriptorExistsException e) {
+    ErrorResponseBody error = new ErrorResponseBody();
+    error.setStatus(e.getErrorStatus());
+    error.setOrigin(e.getErrorOrigin());
+    error.setDetail(e.getMessage());
+    return error;
+  }
+
 }

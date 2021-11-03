869d12569f70b25fe156e776bfd64f4c147c2eb9
joanasoaresd <joana.darck@dcx.ufpb.br>
2021-11-03 02:29:25 -0300

    #13 acrescentando findByName no repository de descritores

--- src/main/java/br/ufpb/edoe/repository/DescriptorRepository.java
+++ src/main/java/br/ufpb/edoe/repository/DescriptorRepository.java
@@ -1,10 +1,13 @@
 package br.ufpb.edoe.repository;
 
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.ufpb.edoe.entity.Descriptor;
 
 @Repository
 public interface DescriptorRepository extends JpaRepository<Descriptor, Integer> {
-    
+    Optional<Descriptor> findByName(String name);
 }

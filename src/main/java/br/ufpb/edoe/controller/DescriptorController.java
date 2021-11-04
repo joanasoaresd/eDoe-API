b3305eca134707fa86fab0de776f5e67bfa30a1b
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-11-03 22:31:42 -0300

    #17 Adição do endpoint para listar os descritores cadastrados

--- src/main/java/br/ufpb/edoe/controller/DescriptorController.java
+++ src/main/java/br/ufpb/edoe/controller/DescriptorController.java
@@ -1,10 +1,14 @@
 package br.ufpb.edoe.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufpb.edoe.dto.DescriptorDTO;
@@ -16,10 +20,15 @@ public class DescriptorController {
 
     @Autowired
     private DescriptorService service;
-    
-    @PostMapping("/descriptors")
-    public ResponseEntity<DescriptorDTO> addDescriptor(@RequestBody Descriptor descriptor) {        
-        return new ResponseEntity<>(service.addDescriptor(descriptor), HttpStatus.CREATED);        
+
+    @GetMapping("/descriptors")
+    public ResponseEntity<List<Descriptor>> getAllDescriptors(@RequestParam("sort") String ordenation) {
+        return new ResponseEntity<>(service.getAllDescriptors(ordenation), HttpStatus.OK);
     }
-    
+
+    @PostMapping("/descriptors")
+    public ResponseEntity<DescriptorDTO> addDescriptor(@RequestBody Descriptor descriptor) {
+        return new ResponseEntity<>(service.addDescriptor(descriptor), HttpStatus.CREATED);
+    }
+
 }

14b72a4ad215d56471bf8f092692a243fbf38736
joanasoaresd <joana.darck@dcx.ufpb.br>
2021-11-09 02:50:14 -0300

    #21 Anotaçao para DescriptorDTO ser usado como objeto  na entidade item

--- src/main/java/br/ufpb/edoe/dto/DescriptorDTO.java
+++ src/main/java/br/ufpb/edoe/dto/DescriptorDTO.java
@@ -1,12 +1,18 @@
 package br.ufpb.edoe.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 import br.ufpb.edoe.entity.Descriptor;
 import lombok.Data;
 
 @Data
+@Embeddable
 public class DescriptorDTO {
 
+    @Column(name = "id_descriptor")
     private int id;
+    @Column(name = "name")
     private String name;
 
     public DescriptorDTO(Descriptor d){
